c996fab582fedaeeaca30c849390ef21dfabda1e
Erik Weir <erik.weir@key.me>
2020-03-27 17:50:01 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,3 +1,5 @@
+
+
 require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
@@ -6,6 +8,4 @@ describe "current_age_for_birth_year method" do
 
     expect(age_of_person).to eq(19)
   end
-end
-def current_age_for_birth_year
 end
